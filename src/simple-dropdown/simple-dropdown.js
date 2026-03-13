b7b304b124de5c1a984f17f39f9b6851092cdc1a
Dmitriy Nesterkin <dmitriy.nesterkin@gmail.com>
2026-03-13 13:25:16 -0400

    Build demo

--- src/simple-dropdown/simple-dropdown.js
+++ src/simple-dropdown/simple-dropdown.js
@@ -278,6 +278,10 @@ export class SimpleDropdown extends FormHTMLElement {
     });
   }
 
+  #handlePointerDown() {
+    
+  }
+
   #addSelectionsWrapperElListeners() {
     let unsubscribeMousemove = addEventListener(this.#selectionsWrapperEl, 'mousemove', (event) => {
       let {clientX, clientY} = event;
@@ -289,7 +293,8 @@ export class SimpleDropdown extends FormHTMLElement {
       }
     });
 
-    this.#trap.c = addEventListener(this.#selectionsWrapperEl, 'pointerdown', () => {
+    this.#trap.c = addEventListener(this.#selectionsWrapperEl, 'pointerdown', (event) => {
+      alert(event.pointerType);
       this.#preventSelectorBlur = true;
     });
 
